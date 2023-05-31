08d9f2cec7ae11afe047a9f7cb1fdbab52f84255
kaia24 <86907898+kaia24@users.noreply.github.com>
2023-05-31 14:57:59 -0700

    Add files via upload

---
+++ likes/index.js
@@ -0,0 +1,24 @@
+var like1=9;
+function add1(){
+    
+var likeElement= document.querySelector("#like1");
+    like1++
+    likeElement.innerText=like1+"like";
+    console.log(like);
+}
+var like2=12;
+function add2(){
+    
+var likeElement= document.querySelector("#like2");
+    like2++
+    likeElement.innerText=like2+"like";
+    console.log(like2);
+}
+var like3=9;
+function add3(){
+    
+var likeElement= document.querySelector("#like3");
+    like3++
+    likeElement.innerText=like3+"like";
+    console.log(like3);
+}
